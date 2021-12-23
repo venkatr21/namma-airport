950f159205c7c6340081d2acd80bee82cc1f2323
Venkat Ragavan <mail2venkat21@gmail.com>
2021-12-23 13:52:14 +0530

    init server.js

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const compression = require('compression');
+const path = require('path');
+const config = require('config');
+
+const app = express();
+
+// middleware setup
+app.use(bodyParser.json());     //usage of body parser
+app.use(cors());                //allow cross origin reference
+app.use(compression());         //compress all the route responses
+
+// DB configuration
+const mongoConnectionString = config.get('AzureCosmosMongoConnectionString');
+// database connection
+mongoose.connect(mongoConnectionString, { useUnifiedTopology: true, useNewUrlParser: true})
+    .then(()=> console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
+
+
+// port for connection
+const port = process.env.PORT || 5000;
+
+// listen for connection
+app.listen(port, () => console.log("Server started at "+port));
+
+app.get("*",(req,res)=>{
+    res.json({"server": "NammaAirport"})
+})
