c3eb006da633a5fc74ebba1a66877bb066350bc8
Venkat Ragavan <mail2venkat21@gmail.com>
2021-12-25 11:17:21 +0530

    chatbot store messages on azure cosmos

--- frontend/src/screens/ChatScreen.js
+++ frontend/src/screens/ChatScreen.js
@@ -9,49 +9,57 @@ import axios from 'axios';
 
 export function ChatScreen ({ navigation, userInfo }) {
     const [messages, setMessages] = useState([]);
-    
+    const [isNewConversation, serNewConversation] = useState(true);
+
+    var botComposeInitialMessage = {
+        _id: uuid.v4(),
+        text: 'Hello '+userInfo.user.givenName+', How can i help you today?',
+        createdAt: new Date(),
+        user: {
+            _id: 'bot',
+            name: 'Namma Airport Bot',
+            avatar: require('../../assets/bot.jpg'),
+        }
+    }
     
     const onLoadEarlier = async()=>{
-        console.log(NAMMA_AIRPORT_SERVER+'messages/'+userInfo.user.email)
-        axios.get('http://2f59-183-82-26-168.ngrok.io/messages/'+userInfo.user.email)
+        axios.get(NAMMA_AIRPORT_SERVER+'messages/'+userInfo.user.email)
         .then(response => {
-            try{
-            setMessages(response.data);
-            }catch{
-
-            }
+            let tempMessages = response.data.reverse();
+            //console.log(response.data)
+            setMessages(tempMessages);
         })
         .catch(err =>{
             console.log(err);
-            //setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
         });
     }
-
+    
     useEffect(() => {
-        setMessages([
-            {
-                _id: uuid.v4(),
-                text: 'Hello '+userInfo.user.givenName+', How can i help you today?',
-                createdAt: new Date(),
-                user: {
-                    _id: 'bot',
-                    name: 'React Native',
-                    avatar: require('../../assets/bot.jpg'),
-                }
-            }
-        ])
+        if(isNewConversation){ 
+            //console.log(botComposeInitialMessage);
+            //console.log(typeof messages)
+            //messages.push([botComposeInitialMessage]);
+            setMessages([
+                botComposeInitialMessage
+            ])
+        }
     }, []);
 
     const onSend = useCallback((messages = []) => {
-        axios.post(NAMMA_AIRPORT_SERVER+'messages/', JSON.stringify({...messages[0], email: userInfo.user.email}))
-        .then(response => {
-            return response.json();
-        })
-        .then(json=>{
+        var config = {
+            method: 'post',
+            url: NAMMA_AIRPORT_SERVER+'messages/',
+            headers: { 
+              'Content-Type': 'application/json'
+            },
+            data : JSON.stringify({...messages[0], email: userInfo.user.email})
+        };
+        axios(config)
+        .then(response => { 
             setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-            console.log(json);
         })
         .catch(err =>{
+            console.log(err);
             setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
         });
     }, []);
@@ -65,9 +73,9 @@ export function ChatScreen ({ navigation, userInfo }) {
             loadEarlier={true}
             onLoadEarlier={()=>{onLoadEarlier()}}
             user={{
-                _id: 1,
-                name: 'React Native',
-                avatar: 'https://placeimg.com/140/140/any',
+                _id: 'user',
+                name: userInfo.user.name,
+                avatar: userInfo.user.photo,
             }}
         />
     );
