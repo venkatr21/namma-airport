2f11af060bbb0dedfc458cccbdcfa93bd88da380
Venkat Ragavan <mail2venkat21@gmail.com>
2021-12-26 22:11:54 +0530

    added search bar

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,18 @@
-import * as React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import {Text, View, StyleSheet, StatusBar} from 'react-native';
 import { TabBar } from '../components/TabBar';
+import SearchBarWithAutocomplete from '../components/SearchBar';
 export function HomeScreen({userInfo}) {
+  const [search, setSearch] = useState({ term: '' })
   return (
     <View style={styles.container}>
         <StatusBar showHideTransition='slide' barStyle='default' backgroundColor="#e91e63"/>
         <TabBar displayText={"Home"} />
-        <View style={{flex: 0.9, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{color: '#000'}}>Hello {userInfo.user.name}</Text>
+        <View style={{flex: 0.9, justifyContent: 'center'}}>
+        <SearchBarWithAutocomplete
+            value={search.term}
+            onChangeText={(text) => setSearch({ term: text })}
+        />
         </View>
     </View>
   );
