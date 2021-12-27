62b47c544131fea6fe92cc27b42a9274be5e8642
Venkat Ragavan <mail2venkat21@gmail.com>
2021-12-27 09:31:36 +0530

    added map markers progress#

--- frontend/src/screens/MapScreen.js
+++ frontend/src/screens/MapScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {View, StyleSheet, StatusBar, ActivityIndicator, KeyboardAvoidingView} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import MapConstants from '../constants/MapConstants';
 import Theme from '../constants/Theme';
 import { TabBar } from '../components/TabBar';
@@ -9,7 +9,15 @@ import SearchBarWithAutocomplete from '../components/SearchBar';
 export function MapScreen({navigation}) {
     const [mapCoordinates, setMapCoordinates] = useState(MapConstants);
     const [isLoading, setIsLoading] = useState(true);
-    const [search, setSearch] = useState({ term: '' })
+    const [search, setSearch] = useState({ term: '' });
+    var searchResult = [{
+      name: "too",
+      position: {lat: 13.203786 ,lon: 77.70643}
+    },
+    {
+      name: "too1",
+      position: {lat: 13.199534,lon: 77.709604}
+    }]
     React.useEffect(() => {
       console.log(search);
         setIsLoading(true);
@@ -24,13 +32,22 @@ export function MapScreen({navigation}) {
           {isLoading ? (<ActivityIndicator color={Theme.activityIndicatorColour} size={"large"} />):(
           <MapView
             style={{flex: 1,width: '100%'}}
+            showsUserLocation={true}
             initialRegion={{
               latitude: mapCoordinates.Latitude,
               longitude: mapCoordinates.Longitude,
               latitudeDelta: mapCoordinates.LatitudeDelta,
               longitudeDelta: mapCoordinates.LongitudeDelta,
-            }}
-          />
+            }}>
+            {searchResult.map((marker, index) => (
+              <Marker
+                key={index}
+                coordinate={{latitude: marker.position.lat, longitude: marker.position.lon}}
+                title={marker.name}
+                description={null}
+              />
+            ))}
+          </MapView>
           )}
           <View style={styles.searchView}>
             <SearchBarWithAutocomplete
