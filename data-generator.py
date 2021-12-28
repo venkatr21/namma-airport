7fa2fe11a100d8e31e1242a7e0a3025169c9b677
Venkat Ragavan <mail2venkat21@gmail.com>
2021-12-28 15:54:32 +0530

    added data-generator pyfile

---
+++ data-generator.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+f = open('./airport-data.json')
+data = json.load(f)
+
+baseUrl = "https://www.bengaluruairport.com"
+baseFileName = "./frontend/assets/imgs/"
+
+
+for i in data["places"]:
+    r = requests.get(baseUrl+i["logo"]).content
+    fileName = i["title"]+".jpg"
+    with open(baseFileName+fileName,"wb+") as f:
+        f.write(r)
