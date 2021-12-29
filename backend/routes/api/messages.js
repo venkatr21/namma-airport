6271ecf1f67c94d552ceb58de2f63ddc91b40dff
Venkat Ragavan <mail2venkat21@gmail.com>
2021-12-29 14:14:16 +0530

    orchestration api design progress#

--- backend/routes/api/messages.js
+++ backend/routes/api/messages.js
@@ -8,6 +8,7 @@ const Message = require(path.join(__dirname,'..','..', 'models','MessageData'));
 
 const botEndpoint = config.get('BotEndpoint');
 const botEndpointAuth = config.get('BotEndpointAuth');
+const luisPredictionEndpoint = config.get('AzureLuisPredictionEndpoint');
 
 function generateBotReplyForAnswer(answer, requestBody){
     var botReply = {
@@ -22,6 +23,80 @@ function generateBotReplyForAnswer(answer, requestBody){
         _id: uuid.v4()
     };
     return botReply;
+}
+
+function generateLuisReplyForAnswer(answer, requestBody){
+    var botReply = {
+        text: answer,
+        createdAt: new Date().toISOString(),
+        user:{
+            _id: "bot",
+            name: "Namma Airport Bot",
+            avatar: "https://nammaairportstorage.blob.core.windows.net/namma-airport/bot.jpg",
+        },
+        email: requestBody.email,
+        _id: uuid.v4()
+    };
+    return botReply;
+}
+
+async function fetchBotResponse(question){
+    var data = JSON.stringify({
+        "question": question
+    });
+    var config = {
+        method: 'post',
+        url: botEndpoint,
+        headers: { 
+          'Authorization': botEndpointAuth, 
+          'Content-Type': 'application/json',
+        },
+        data : data
+    };
+    var response = await axios(config);
+    try{
+        return response.data;
+    }catch{
+        return null;
+    }
+}
+
+async function fetchLuisResponse(question){
+    var data = JSON.stringify({
+        "query": question
+    });
+    var config = {
+        method: 'post',
+        url: luisPredictionEndpoint,
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        data : data
+    };
+    var response = await axios(config);
+    try{
+        return response.data;
+    }catch{
+        return null;
+    }
+}
+
+async function orchestrateAnswer(question, requestBody){
+    const botResponse = await fetchBotResponse(question);
+    const luisResponse = await fetchLuisResponse(question);
+
+    const botScore = botResponse.answers[0].score;
+    const luisTopIntent = luisResponse.prediction.topIntent;
+    if(luisTopIntent!=="None"){
+        const luisScore = luisResponse.prediction.intents[luisTopIntent].score;
+        if(luisScore*100>botScore){
+            return "Luis Response"
+        }else{
+            return "Bot Response"
+        }
+    }else{
+        return "Bot Response"
+    }
 }
 
 async function fetchAnswer(question, requestBody){
@@ -39,6 +114,7 @@ async function fetchAnswer(question, requestBody){
     };
     var response = await axios(config);
     try{
+        console.log(response.data)
         return generateBotReplyForAnswer(response.data.answers[0].answer, requestBody);
     }catch{
         return null;
@@ -69,6 +145,18 @@ router.post('/',async (req,res)=>{
     .catch((err)=>{
         res.sendStatus(400);
     })
+})
+
+router.post('/test',async (req,res)=>{
+    var question = req.body.text;
+    orchestrateAnswer(question)
+    .then(response=>{
+        res.json({"reply from": response});
+    })
+    .catch(err=>{
+        res.sendStatus(400);
+    })
+    
 })
 
 
