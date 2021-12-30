58668365a92cf590c59c723f738d4141e2510ee8
Venkat Ragavan <mail2venkat21@gmail.com>
2021-12-30 18:45:24 +0530

    added homescreen services

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,10 @@ import {Text, View, StyleSheet, StatusBar, ScrollView, SafeAreaView} from 'react
 import { TabBar } from '../components/TabBar';
 import { KioskView } from '../components/KioskView';
 import HomePageCarousel from '../components/HomePageCarousel';
+import Dim from '../constants/Dimensions';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Theme from '../constants/Theme';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 export function HomeScreen({userInfo, navigation}) {
   return (
     <SafeAreaView  style={styles.container}>
@@ -16,6 +20,52 @@ export function HomeScreen({userInfo, navigation}) {
           <View style={styles.kioskContainer}>
             <Text style={styles.homepageSectionText}>Self-serve E-Kiosk</Text>
             <KioskView />
+          </View>
+          <View style={styles.kioskContainer}>
+            <Text style={styles.homepageSectionText}>Services for you</Text>
+            <View style={styles.homeScreenServices}>
+              <View style={styles.serviceViewRow}>
+                <View style={styles.serviceViewColumn}>
+                  <TouchableOpacity style={styles.serviceIcon}>
+                    <Icon color={Theme.mainColour} name={'flight'} size={35} />
+                  </TouchableOpacity>
+                  <Text style={styles.serviceTitle}>Boarding</Text>
+                </View>
+                <View style={styles.serviceViewColumn}>
+                  <TouchableOpacity style={styles.serviceIcon}>
+                    <Icon color={Theme.mainColour} name={'remove-shopping-cart'} size={35} />
+                  </TouchableOpacity>
+                  <Text style={styles.serviceTitle}>Hello</Text>
+                </View>
+                <View style={styles.serviceViewColumn}>
+                  <TouchableOpacity style={styles.serviceIcon}>
+                    <Icon color={Theme.mainColour} name={'fastfood'} size={35} />
+                  </TouchableOpacity>
+                  <Text style={styles.serviceTitle}>Culinary</Text>
+                </View>
+              </View>
+
+              <View style={styles.serviceViewRow}>
+                <View style={styles.serviceViewColumn}>
+                  <TouchableOpacity style={styles.serviceIcon}>
+                    <Icon color={Theme.mainColour} name={'flight'} size={35} />
+                  </TouchableOpacity>
+                  <Text style={styles.serviceTitle}>Hello</Text>
+                </View>
+                <View style={styles.serviceViewColumn}>
+                  <TouchableOpacity style={styles.serviceIcon}>
+                    <Icon color={Theme.mainColour} name={'wifi'} size={35} />
+                  </TouchableOpacity>
+                  <Text style={styles.serviceTitle}>Airport Wifi</Text>
+                </View>
+                <View style={styles.serviceViewColumn}>
+                  <TouchableOpacity style={styles.serviceIcon}>
+                    <Icon color={Theme.mainColour} name={'wheelchair-pickup'} size={35} />
+                  </TouchableOpacity>
+                  <Text style={styles.serviceTitle}>Assistance</Text>
+                </View>
+              </View>
+            </View>
           </View>
         </ScrollView>
     </SafeAreaView >
@@ -39,4 +89,30 @@ const styles = StyleSheet.create({
     kioskContainer:{
       marginTop: 10,
     },
+    homeScreenServices:{
+      width: Dim.WindowWidth-20,
+      marginLeft: 10,
+    },
+    serviceViewRow:{
+      marginVertical: 10,
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-around'
+    },
+    serviceTitle:{
+      color: 'black',
+      marginTop: 5,
+      fontFamily: 'Lato-Italic',
+      fontSize: 13,
+    },
+    serviceViewColumn: {
+      alignItems: 'center',
+    },
+    serviceIcon:{
+      alignSelf: 'center',
+      borderRadius: 100,
+      padding: 10,
+      backgroundColor: '#ddd',
+      color: Theme.mainColour
+    }
 });
